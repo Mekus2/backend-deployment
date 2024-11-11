49014c704fed16b5835b953c83cba8fa6dd9f431
Mekus2 <purposescapstone@gmail.com>
2024-11-11 15:58:31 +0800

    modified models and gitignore

--- backend/Admin/Customer/models.py
+++ backend/Admin/Customer/models.py
@@ -5,7 +5,7 @@ class Clients(models.Model):
     name = models.CharField(max_length=255, blank=False, null=False)
     address = models.CharField(max_length=255, blank=False, null=False)
     province = models.CharField(max_length=255, blank=False, null=False)
-    phoneNumber = models.CharField(max_length=15, blank=False, null=False)
+    phoneNumber = models.CharField(max_length=15, blank=True, null=True)
 
     def __str__(self):
         return self.name
