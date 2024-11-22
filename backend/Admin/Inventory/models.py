79fb7d8160b90cfcbaa010d2dbda66b44eecc885
Mekus2 <purposescapstone@gmail.com>
2024-11-22 10:06:34 +0800

    Changes in Inventory models

--- backend/Admin/Inventory/models.py
+++ backend/Admin/Inventory/models.py
@@ -1,3 +1,4 @@
+import datetime
 from django.db import models
 
 from Admin.Product.models import Product
@@ -23,13 +24,32 @@ class Inventory(models.Model):
     def save(self, *args, **kwargs):
         if not self.INVENTORY_ID:
             # Generate the next INVENTORY_ID
-            last_entry = Inventory.objects.order_by("-INVENTORY_ID").first()
-            if last_entry:
-                last_id = int(last_entry.INVENTORY_ID[3:])  # Extract numeric part
+            last_inventory = Inventory.objects.order_by("-INVENTORY_ID").first()
+            if last_inventory:
+                last_id = int(last_inventory.INVENTORY_ID[3:])  # Extract numeric part
                 new_id = f"INV{last_id + 1:05d}"  # Increment and format
             else:
                 new_id = "INV00001"  # Start ID
             self.INVENTORY_ID = new_id
+
+        if not self.BATCH_ID:
+            # Generate a BATCH_ID starting with MM-YY + ID
+            current_date_prefix = datetime.now().strftime("%m-%y")
+            last_batch = (
+                Inventory.objects.filter(BATCH_ID__startswith=current_date_prefix)
+                .order_by("-BATCH_ID")
+                .first()
+            )
+
+            if last_batch:
+                last_batch_number = int(
+                    last_batch.BATCH_ID.split("-")[-1]
+                )  # Extract numeric part after MM-YY
+                new_batch_id = f"{current_date_prefix}-{last_batch_number + 1:03d}"  # Increment and format
+            else:
+                new_batch_id = f"{current_date_prefix}-001"  # Start with MM-YY-001
+            self.BATCH_ID = new_batch_id
+
         super().save(*args, **kwargs)
 
     def __str__(self):
