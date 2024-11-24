06e6cc02dd21958a0532921babb2846495bfa2fd
edieee1 <ediebasay21@gmail.com>
2024-11-24 13:37:11 +0800

    deac and activate with user logs for both

--- backend/Account/urls.py
+++ backend/Account/urls.py
@@ -3,7 +3,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import (
     RegisterView, UserListView, UpdateUserView, CheckAuthenticationView, 
     UserProfileImageView, ChangePasswordView, TotalStaffCount, TotalActiveUserCount, UserListExcludeSuperAdminView,
-    DeactivateUserView, ReactivateUserView,
+    DeactivateUserView, ReactivateUserView,DeactivatedUserListView,ActivatedUserListView, UserDetailView,
+    UserLogs
 )
 
 urlpatterns = [
@@ -17,14 +18,26 @@ urlpatterns = [
     path('check-auth/', CheckAuthenticationView.as_view(), name='check-auth'),
     path('users/<int:user_id>/image/', UserProfileImageView.as_view(), name='user-profile-image'),
 
-    #returns all data except accType = superadmin
+#returns all data except accType = superadmin
     path('lists/',UserListExcludeSuperAdminView.as_view(), name='Total Staff'),
 
+    #Path for user logs
+    path('logs/',UserLogs.as_view(), name='User logs'),
+    path('logs/<int:user_id>/', UserLogs.as_view(), name='user Logs pk'),
+    
     #Total Staff
     path('totalStaff/',TotalStaffCount.as_view(), name='Total Staff'),
 
     #Total Active Staff
     path('totalActiveStaff/',TotalActiveUserCount.as_view(), name='Total Active Staff'),
+
+    #Return all deactivated and active users:
+    path('deactivated/', DeactivatedUserListView.as_view(), name='deactivated-users'),
+    path('activated/', ActivatedUserListView.as_view(), name='Active users'),
+
+    #path for details
+    path('details/', UserDetailView.as_view(), name='details users'),
+    path('details/<int:user_id>/', UserDetailView.as_view(), name='user-detail'),
 
     #To Deactivate and reactivate user
     path('deactivateUser/<int:user_id>/',DeactivateUserView.as_view(), name='Deactivate user'),
