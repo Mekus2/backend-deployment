f36aa88900ffc28e4b88ff5d4faa758b90324da4
NavaresMaryPrecious <navaresmaryprecious@gmail.com>
2025-01-08 22:49:45 +0800

    secret key

--- backend/PHILVETS/settings.py
+++ backend/PHILVETS/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-h+1l4!9y38u5*ehn#6g3^na3cs4+b*0*0x9-v+mhw##a=!9&b!")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
